736cc4c5c61d089279ac0586bdeac4a5400a705f
samirm00 <sammoham.be@gmail.com>
2023-06-27 15:28:44 +0200

    principles and enemies

---
+++ 2-clean-code-principles-enemies/README.md
@@ -0,0 +1,32 @@
+# Clean code principle and enemies
+
+## Principles
+
+### Reliability
+
+- a reliable code means it is `correct` , `stable` and `resilient`.
+  
+#### correct
+
+- the code is considered correct it is able to fulfill the requirements
+- [validate email address](./validate-email.test.js)
+
+### Efficiently
+
+### Maintainability
+
+### Usability
+
+## Enemies
+
+### JavaScript
+
+### Management
+
+### Self
+
+### the cargo cult
+
+## References
+
+- Padolsey James, Clean Code in JavaScript, (Packt Publishing Ltd, 2020).
---
+++ 2-clean-code-principles-enemies/validate-email.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * validateEmail :  validate email address
+ * @param {string} email- - email to validate
+ * @returns {boolean} is true for validate email or false
+ */
+
+
+const validateEmail = (email) => {
+  const EmailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return EmailPattern.test(email);
+};
+
+describe('validateEmail', () => {
+  test('should return true for a valid email address', () => {
+    const validEmail = 'john.doe@example.com';
+    expect(validateEmail(validEmail)).toBe(true);
+  });
+
+  test('should return false for an email without @ symbol', () => {
+    const invalidEmail = 'invalid.email.com';
+    expect(validateEmail(invalidEmail)).toBe(false);
+  });
+
+  test('should return false for an email without domain', () => {
+    const invalidEmail = 'john.doe@';
+    expect(validateEmail(invalidEmail)).toBe(false);
+  });
+
+  test('should return false for an email without top-level domain', () => {
+    const invalidEmail = 'john.doe@example';
+    expect(validateEmail(invalidEmail)).toBe(false);
+  });
+});
